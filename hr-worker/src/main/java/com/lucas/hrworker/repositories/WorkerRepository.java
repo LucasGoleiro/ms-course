af6f1a467ff4aeb09265f263f548e37723b7d70a
Lucas Coelho dos Santos <lucas.goleiro1995@gmail.com>
2022-03-21 19:22:11 -0300

    Work implemented

---
+++ hr-worker/src/main/java/com/lucas/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.lucas.hrworker.repositories;
+
+import com.lucas.hrworker.entities.Worker;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long>{
+
+}
